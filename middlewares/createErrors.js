1269eb7a005897c2bd704951f4e58153b35592c1
nellolilu <nelli.neb@gmail.com>
2021-04-14 21:55:05 +0200

    notes, go home links, default enum, tried middelware for errors

---
+++ middlewares/createErrors.js
@@ -0,0 +1,101 @@
+module.exports = (req, res, next) => {
+  if (name.length < 3) {
+    res.render("create-company", {
+      errorMessage: "please add your companies name",
+    });
+    return;
+  }
+  if (webpage.length < 16) {
+    res.render("create-company", {
+      errorMessage: "please fill in a complete url",
+    });
+    return;
+  }
+  // to do: error messages  for email does not match
+  if (email.length < 8) {
+    res.render("create-company", {
+      errorMessage: "please fill in a complete emailadress",
+    });
+    return;
+  }
+  if (adress.length < 4) {
+    res.render("create-company", {
+      errorMessage: "please share your adress",
+    });
+    return;
+  }
+  if (size.length < 1) {
+    res.render("create-company", {
+      errorMessage: "please pick a size for your company",
+    });
+    return;
+  }
+  if (branch.length < 1) {
+    res.render("create-company", {
+      errorMessage: "please pick a branch for your company",
+    });
+    return;
+  }
+  if (social1 < 1 || ecological1 < 1 || economic1 < 1) {
+    res.render("create-company", {
+      errorMessage: "are you sure you dont want to go transparent?",
+    });
+    return;
+  }
+  next();
+};
+
+// function checkErrors() {
+// if (name.length < 3) {
+//   // alert("howdy");
+//   res.render("create-company", {
+//     errorMessage: "please add your companies name",
+//   });
+//   return;
+// }
+// if (webpage.length < 16) {
+//   // alert("howdy");
+//   res.render("create-company", {
+//     errorMessage: "please fill in a complete url",
+//   });
+//   return;
+// }
+// // to do: error messages  for email does not match
+// if (email.length < 8) {
+//   // alert("howdy");
+//   res.render("create-company", {
+//     errorMessage: "please fill in a complete emailadress",
+//   });
+//   return;
+// }
+// if (adress.length < 4) {
+//   // alert("howdy");
+//   res.render("create-company", {
+//     errorMessage: "please share your adress",
+//   });
+//   return;
+// }
+// if (size.length < 1) {
+//   // alert("howdy");
+//   res.render("create-company", {
+//     errorMessage: "please pick a size for your company",
+//   });
+//   return;
+// }
+// if (branch.length < 1) {
+//   // alert("howdy");
+//   res.render("create-company", {
+//     errorMessage: "please pick a branch for your company",
+//   });
+//   return;
+// }
+// // only works if the input stays
+// if (social1 < 1 || ecological1 < 1 || economic1 < 1) {
+//   res.render("create-company", {
+//     errorMessage: "are you sure you dont want to go transparent?",
+//   });
+//   return;
+//   // }
+// }
+
+// // module.exports = checkErrors();
--- routes/create-company.js
+++ routes/create-company.js
@@ -1,3 +1,17 @@
+// TO DOS THURSDAY (FRIDAY)
+// - pictureupload
+// - edit & delete (need a user!!)
+// - Bugfix errormessages & middelware
+
+// TO SET UO SATURDAY
+// - Index, Profile, Search
+
+// MON & TUE
+// - additionals: Network/ rating / Proofpicture
+
+// TUE NIGHT
+// - set up CSS looks
+
 // ******** USEFUL LATER
 // const isLoggedMiddleware = require("../middlewares/MustBeLoggedIn");
 // const slugify = require("slugify");
@@ -5,17 +19,25 @@
 // ********* SERIOUS BUGS:
 // DOES THIS ONLY WORK IF AUTH FINISHED?
 // console.log("*******req.session.user:", req.session.user);
+// after errorMessage select is empty, message doent go away, ENUMS broken, also in edit
 
 // ********* NICE TO SOLVE BUGS:
-// after errorMessage select is empty, message doent go away
-// make select and branch by default other, and name as placeholder
+
+//ERROR MESSAGES
+
+// middelware does not work
 // May exchange errormessage to alerts? to keep contents
+// enum required message breaks.
+
+// CHECK EMAIL FOR COMANY
+
+// GO BACK statt GO HOME
 
 const SIZE_ENUM = require("../utils/size-enum");
 const BRANCH_ENUM = require("../utils/branch-enum");
 const Company = require("../models/Company.model");
 const User = require("../models/User.model");
-
+// const createErrors = require("../middlewares/createErrors");
 const express = require("express");
 
 // //test didnt work
@@ -43,25 +65,67 @@ router.post("/", (req, res) => {
     ecological1,
     economic1,
   } = req.body;
-  console.log("this is the email:", email);
 
-  // to do: error messages  for email does not match
+  // // to do: error messages  for email does not match
 
-  if (adress.length < 4) {
-    // alert("howdy");
-    res.render("create-company", {
-      errorMessage: "please share your adress",
-    });
-    return;
-  }
+  // if (name.length < 3) {
+  //   // alert("howdy");
+  //   res.render("create-company", {
+  //     errorMessage: "please add your companies name",
+  //   });
+  //   return;
+  // }
 
-  // only works if the input stays
-  if (social1 < 1 || ecological1 < 1 || economic1 < 1) {
-    res.render("create-company", {
-      errorMessage: "are you sure you dont want to go transparent?",
-    });
-    return;
-  }
+  // if (url.length < 16) {
+  //   // alert("howdy");
+  //   res.render("create-company", {
+  //     errorMessage: "please fill in a complete url",
+  //   });
+  //   return;
+  // }
+
+  // // to do: error messages  for email does not match
+  // if (email.length < 8) {
+  //   // alert("howdy");
+  //   res.render("create-company", {
+  //     errorMessage: "please fill in a complete emailadress",
+  //   });
+  //   return;
+  // }
+
+  // if (adress.length < 4) {
+  //   // alert("howdy");
+  //   res.render("create-company", {
+  //     errorMessage: "please share your adress",
+  //   });
+  //   return;
+  // }
+
+  // if (size.length < 1) {
+  //   // alert("howdy");
+  //   res.render("create-company", {
+  //     errorMessage: "please pick a size for your company",
+  //   });
+  //   return;
+  // }
+
+  // if (branch.length < 1) {
+  //   // alert("howdy");
+  //   res.render("create-company", {
+  //     errorMessage: "please pick a branch for your company",
+  //   });
+  //   return;
+  // }
+
+  // // to do: error messages  for email does not match
+
+  // // only works if the input stays
+  // if (social1 < 1 || ecological1 < 1 || economic1 < 1) {
+  //   res.render("create-company", {
+  //     errorMessage: "are you sure you dont want to go transparent?",
+  //   });
+  //   return;
+  // }
 
   Company.findOne({ url }).then((found) => {
     if (found) {
@@ -80,7 +144,7 @@ router.post("/", (req, res) => {
       social1,
       ecological1,
       economic1,
-      owner: req.session.user._id,
+      // owner: req.session.user._id,
     }).then((createdCompany) => {
       console.log("created company:", createdCompany);
       res.redirect("/");
@@ -88,11 +152,10 @@ router.post("/", (req, res) => {
   });
 });
 
-
-
 // THE ROUTER BELONGS INTO PROFILE ROUTE
 // FIRST AUTH; THEN FINISH
 
+// NEED A PARAMS FOR ID CHECK
 router.get("/edit-company", (req, res) => {
   Company.find({}).then((allCompanies) => {
     console.log("this is all you got", allCompanies);
@@ -100,7 +163,7 @@ router.get("/edit-company", (req, res) => {
     // REQ SESSION USER IS UNDEFINED
     console.log(req.session);
     res.render("edit-company", {
-      company: allCompanies[0], 
+      company: allCompanies[0],
       branch: BRANCH_ENUM,
       size: SIZE_ENUM,
     });
